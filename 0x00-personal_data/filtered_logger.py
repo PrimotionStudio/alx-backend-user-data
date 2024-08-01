bc85c406a08a9e5c88d1d7163d2e90ba3690406f
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-01 23:34:16 +0100

    "Refactored filter_datum function in filtered_logger.py to iterate over message instead of new_str, and updated variable assignments accordingly."

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,9 +9,9 @@ def filter_datum(fields, redaction, message, separator):
     """
     This function filters the data from the log message.
     """
-    new_str = message
-    for p in re.split(";", new_str):
+    for p in re.split(";", message):
         field = re.split("=", p)
         if field[0] in fields:
-            new_str = re.sub(f"{field[0]}={field[1]}", f"{field[0]}={redaction}", new_str)
-    return new_str
+            message = re.sub(f"{field[0]}={field[1]}",
+                             f"{field[0]}={redaction}", message)
+    return message
