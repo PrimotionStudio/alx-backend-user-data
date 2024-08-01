90c7e40fbc9a8ea694f439b5a81a71eaeb40effc
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-01 22:41:54 +0100

    "Update filtered_logger.py: changed shebang line, added docstring, and implemented filter_datum function"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function to obfuscate logs.
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    This function filters the data from the log message.
+    """
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
+    return re.sub(pattern, lambda x: x.group(0).split('=')[0] + '=' + redaction, message)
