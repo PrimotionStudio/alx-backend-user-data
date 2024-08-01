9b3761c2da555feeeb517007ae41f5791ab38198
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-01 22:45:40 +0100

    Remove unnecessary variable assignment; inline pattern definition in re.sub call.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,6 @@ def filter_datum(fields, redaction, message, separator):
     """
     This function filters the data from the log message.
     """
-    pattern = f"({'|'.join(fields)})=[^{separator}]*"
-    return re.sub(pattern,
+    return re.sub(f"({'|'.join(fields)})=[^{separator}]*",
                   lambda x: x.group(0).split('=')[0] + '=' + redaction,
                   message)
