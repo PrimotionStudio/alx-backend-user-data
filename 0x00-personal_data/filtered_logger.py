fdbf9cd5bbc0694f9784b48258c9a9cb4746042b
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-02 00:19:17 +0100

    "Updated filtered_logger module: added PII_FIELDS tuple, get_logger function, and modified RedactingFormatter class."

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,10 @@ This module contains the function to obfuscate logs.
 """
 import re
 import logging
-from typing import List
+from typing import List, Tuple
+
+
+PII_FIELDS: Tuple[str] = ("email", "phone", "ssn", "password", "ip")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -37,3 +40,17 @@ def filter_datum(fields, redaction, message, separator):
             message = re.sub(f"{field[0]}={field[1]}",
                              f"{field[0]}={redaction}", message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and returns a logger with specific configuration.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate(False)
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
