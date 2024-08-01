f1887fdc627080fd39bb8122b2d8e84aef5cac7f
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-02 00:37:22 +0100

    "Updated encrypt_password.py to use env python3, added docstring, and implemented bcrypt password hashing"

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains the function to hash passwords.
+"""
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password with a random salt.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
