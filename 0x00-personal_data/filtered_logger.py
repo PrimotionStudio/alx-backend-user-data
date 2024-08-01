28fcb83353fb0380c21802de0233a2d5e631aa88
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-02 00:01:47 +0100

    "Refactor filtered_logger module, add RedactingFormatter class, and remove main.py file"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,28 @@
 This module contains the function to obfuscate logs.
 """
 import re
+import logging
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_msg = super(RedactingFormatter, self).format(record)
+        filtered = filter_datum(list(self.fields),
+                                RedactingFormatter.REDACTION,
+                                record.msg, RedactingFormatter.SEPARATOR)
+        return re.sub(record.msg, filtered, log_msg)
 
 
 def filter_datum(fields, redaction, message, separator):
