f881974550c1ee83bfb4c350bf2431a43bf16e5b
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-06 00:11:04 +0100

    "Removed shebang lines from __init__.py and basic_auth.py, and added a new Auth class with three methods to auth.py."

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains authentication functions
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    This class contains authentication functions
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        This function checks if a path requires authentication
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """
+        This function gets the authorization header from a request
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        This function gets the current user from a request
+        """
+        return None
