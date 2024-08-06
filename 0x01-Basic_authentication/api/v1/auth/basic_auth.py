e7d1e031abf627352a66c997b25a6ba3ac75131c
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-06 14:10:49 +0100

    "Added method extract_base64_authorization_header to BasicAuth class in basic_auth.py"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,14 @@ class BasicAuth(Auth):
     """
     Basic Auth class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the base64 encoded part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
