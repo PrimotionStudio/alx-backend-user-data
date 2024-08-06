5b1835c53e5b216e7d12b185bdaedfe09f55837f
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-06 14:23:22 +0100

    "Added import for base64 and new method decode_base64_authorization_header to BasicAuth class"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 basic authentication
 """
+from base64 import b64decode
 from .auth import Auth
 
 
@@ -21,3 +22,19 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header):
+        """
+        Decodes the base64 encoded Authorization header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            result = b64decode(base64_authorization_header,
+                               validate=False)
+            return result.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
