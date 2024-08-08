88f0e4d28d3c86c4f1b5bd7a7af300f4a809512d
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-08 02:58:52 +0100

    "Updated SessionAuth class to use instance variable instead of class variable for user_id_by_session_id dictionary."

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return None
         session_id = uuid4()
-        SessionAuth.user_id_by_session_id.update({session_id: user_id})
+        self.user_id_by_session_id.update({session_id: user_id})
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
@@ -28,4 +28,4 @@ class SessionAuth(Auth):
         returns user_id based on session_id"""
         if session_id is None or not isinstance(session_id, str):
             return None
-        return SessionAuth.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
