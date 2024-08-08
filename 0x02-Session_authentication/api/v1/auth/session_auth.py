699efeaf7c60447596c7e1b89480614ca78668e8
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-08 02:54:08 +0100

    "Added new method `user_id_for_session_id` to `SessionAuth` class and updated corresponding pycache file."

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,3 +22,10 @@ class SessionAuth(Auth):
         session_id = uuid4()
         SessionAuth.user_id_by_session_id.update({session_id: user_id})
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        returns user_id based on session_id"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
