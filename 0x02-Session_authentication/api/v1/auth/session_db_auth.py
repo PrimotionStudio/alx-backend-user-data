680279ac093086b43cea52aaf9160f96ff02c2f2
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-13 14:48:17 +0100

    "Updated session_db_auth.py and user_session.py files: changed shebang line, added imports, and implemented create_session method in SessionDBAuth class and __init__ method in UserSession class."

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""".db_User.json"""
+from .session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """.db_User.json"""
+    def create_session(self, user_id=None):
+        """.db_User.json"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+        UserSession.save_to_file()
+        return session_id
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""models/user_session.py"""
+from .base import Base
+
+
+class UserSession(Base):
+    """models/user_session.py"""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
