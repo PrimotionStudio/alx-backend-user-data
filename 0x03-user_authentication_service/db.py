fbfd0a725fdbe011ebdbf66d191cb605669a1e0f
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-14 22:18:25 +0100

    sqlite

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
         """add a user object to db"""
         user = User(email=email, hashed_password=hashed_password)
