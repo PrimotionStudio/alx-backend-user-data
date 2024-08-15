564c7f3f8c73474553dc35e5c29aad444b69a593
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 04:25:29 +0100

    hashed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""hashes a pasword with bcrypt"""
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    function to hasha pwd"""
+    return hashpw(password.encode('utf-8'), gensalt())
