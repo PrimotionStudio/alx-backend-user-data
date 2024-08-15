7305b2bcffe6f92310309ff768c83700da32cb64
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 04:45:12 +0100

    create user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 """hashes a pasword with bcrypt"""
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
+def _hash_password(self, password: str) -> bytes:
     """
     function to hasha pwd"""
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email, password) -> User:
+        """registers a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            if not isinstance(password, bytes):
+                password = self._hash_password(password)
+            return self._db.add_user(email, password)
