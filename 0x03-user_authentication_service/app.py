bad8acad80c00f53034e99fdf454185a254ebbdf
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 04:55:05 +0100

    created a simple flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""a simple flask app"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def bienvenue():
+    """bienvenue"""
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
