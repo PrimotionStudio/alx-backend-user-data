c94b66bc86d3446181ec738971e5846934a81c54
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 05:04:34 +0100

    created a simple flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """a simple flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +12,18 @@ app = Flask(__name__)
 def bienvenue():
     """bienvenue"""
     return jsonify({"message": "Bienvenue"}), 200
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """create a new user using the AUTH module"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        msg = {"email": f"{email}", "message": "user created"}
+        return jsonify(msg), 201
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
