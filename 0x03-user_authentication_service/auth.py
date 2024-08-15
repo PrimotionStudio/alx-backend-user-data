bbf1befaa2b4ccc812790aab44378a72d97c498d
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 04:48:23 +0100

    create user and handle duplicate users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """hashes a pasword with bcrypt"""
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
 
 
-def _hash_password(self, password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """
     function to hasha pwd"""
     return hashpw(password.encode('utf-8'), gensalt())
@@ -25,5 +26,5 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             if not isinstance(password, bytes):
-                password = self._hash_password(password)
+                password = _hash_password(password)
             return self._db.add_user(email, password)
