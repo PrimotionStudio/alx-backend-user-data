c5194c7c1b3fd3241e44dded5f18c460e8af5411
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 17:41:57 +0100

    session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """a simple flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -25,6 +25,21 @@ def users() -> str:
         return jsonify(msg)
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    function to responde to the sessions route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": f"{email}", "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
