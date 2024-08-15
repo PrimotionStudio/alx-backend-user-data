ac3c0ddfbc3991a6f22fcf84802cb8e11dad5383
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 04:13:48 +0100

    task 3: update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,5 +54,13 @@ class DB:
             raise InvalidRequestError
 
     def update_user(self, user_id, **kwargs) -> User:
-        """"""
-        pass
+        """used to update a user"""
+        try:
+            user = self.find_user_by(id=user_id)
+            for k, v in kwargs.items():
+                if not hasattr(user, k):
+                    raise ValueError
+                setattr(user, k, v)
+            self._session.commit()
+        except (ValueError, NoResultError, InvalidRequestError):
+            self._session.rollback()
