1f00b88e795ba56a3d4bd34cdb0f24326bf34fbd
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-15 17:09:29 +0100

    "Updated auth.py to handle NoResultFound exception, and modified main.py to test create_session method instead of valid_login"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,8 +51,11 @@ class Auth:
         """
         creates a session for the user
         """
-        user = self._db.find_user_by(email=email)
-        user_id = user.id
-        session_id = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
+        try:
+            user = self._db.find_user_by(email=email)
+            user_id = user.id
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
